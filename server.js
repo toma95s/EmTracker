adb68d3777aaee53789d7397e42e48d20fdaa268
toma <toma95s@tomas-MBP.lan>
2022-05-01 12:56:02 -0700

    added connection to database, added require api, added fs require, created schema, seeds and added contents

--- server.js
+++ server.js
@@ -1,7 +1,19 @@
-const express = require('express');
-const path = require('path');
+const mysql = require ('mysql2');
+const inquirer = require('inquirer');
 const fs = require("fs")
 //const api = require('./routes/index.js');
+
+
+// Create the connection to database
+const connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3002,
+    user: 'root',
+    // MySQL password
+    password: 'Password123!',
+    database: '' // add database
+  });
+
 
 const PORT = process.env.port || 3002;
 const app = express();
