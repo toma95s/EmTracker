68c71423c8d12e72fcfab12817303b0014b2f958
toma <toma95s@tomas-MBP.lan>
2022-05-01 13:20:38 -0700

    added tables and foreign keys

--- app.js
+++ app.js
@@ -7,18 +7,10 @@ const fs = require("fs")
 // Create the connection to database
 const connection = mysql.createConnection({
     host: 'localhost',
-    port: 3002,
     user: 'root',
     // MySQL password
     password: 'Password123!',
-    database: '' // add database
+    database: 'employeedatabase' // add database
   });
 
-
-const PORT = process.env.port || 3002;
-const app = express();
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-//app.use('/api', api);
 
---
+++ schema.sql
@@ -0,0 +1,27 @@
+DROP DATABASE IF EXISTS employeedatabase;
+CREATE DATABASE employeedatabase;
+USE employeedatabase;
+
+CREATE TABLE department(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30),
+)
+
+CREATE TABLE role(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30),
+    salaray DECIMAL,
+    department_id INT, 
+    FOREIGN KEY(department_id) REFERENCES department(id) 
+)
+
+CREATE TABLE employee(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT,
+    FOREIGN KEY (role_id) REFERENCES role(id)
+    manager_id INT,
+    FOREIGN KEY (manager_id) REFERENCES employee(id) 
+)
+
